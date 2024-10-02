8f1f1424e28ba58055aca25c9688d3ed19435d79
muneeb-babar <muneeb63ahmad63@gmail.com>
2024-10-02 19:10:52 +0500

    Create navbar Ui

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { styled } from '@mui/material/styles';
 import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Avatar from '@mui/material/Avatar';
+import TextField from '@mui/material/TextField';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const drawerWidth = 240;
 
@@ -27,6 +31,17 @@ const AppBar = styled(MuiAppBar, {
 }));
 
 export default function Navbar({ open, handleDrawerOpen }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const openMenu = Boolean(anchorEl);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="fixed" open={open}>
       <Toolbar>
@@ -39,9 +54,59 @@ export default function Navbar({ open, handleDrawerOpen }) {
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" noWrap component="div">
+        <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
           Mini variant drawer
         </Typography>
+
+        {/* Search Input with Custom Color */}
+        <TextField
+          placeholder="Search"
+          variant="outlined"
+          size="small"
+          sx={{
+            width: { xs: '6rem', md: 'auto' },
+            marginRight: 2,
+            '& .MuiOutlinedInput-root': {
+              '& fieldset': {
+                borderColor: 'white', // Change border color
+              },
+              '&:hover fieldset': {
+                borderColor: 'white', // Change hover border color
+              },
+              '&.Mui-focused fieldset': {
+                borderColor: 'white', // Change focused border color
+              },
+            },
+            '& .MuiInputBase-input': {
+              color: 'white', // Change text color
+            },
+          }}
+          InputLabelProps={{
+            style: { color: 'white' }, // Change placeholder color
+          }}
+        />
+
+        {/* Avatar Dropdown */}
+        <IconButton onClick={handleMenuOpen} color="inherit">
+          <Avatar
+            alt="User Avatar"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+          />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={openMenu}
+          onClose={handleMenuClose}
+          PaperProps={{ elevation: 3 }}
+          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        >
+          <MenuItem onClick={handleMenuClose}>
+            Profile <span style={{ marginLeft: 'auto', fontSize: '0.8rem' }}>New</span>
+          </MenuItem>
+          <MenuItem onClick={handleMenuClose}>Settings</MenuItem>
+          <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
